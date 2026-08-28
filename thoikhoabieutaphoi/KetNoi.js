e260ab9cba4c5dd6aa828f1e46b952928c39af2c
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-08-28 08:59:22 +0700

    Add files via upload

---
+++ thoikhoabieutaphoi/KetNoi.js
@@ -0,0 +1,19 @@
+const CAU_HINH_FRONTEND = {
+    URL_API_MAY_CHU: 'https://script.google.com/macros/s/AKfycbwZ2s1lF4ABsD4EQBHOJpTaGieUOtnhTRTgukCOlbMSd96f888IiWUu9ocuZfhc2PxN/exec',
+    TEN_DU_AN: 'THỜI KHÓA BIỂU THÔNG MINH',
+    
+    // Cấu hình chữ ký Footer hiển thị 2 dòng
+    TIEU_DE_TAC_GIA: 'Thiết kế và phát triển',
+    TAC_GIA_THIET_KE: 'Hoàng Ngọc Lâm',
+
+    LINK_LOGO_TRANG_CHU: 'https://i.postimg.cc/xjzpvXCm/Logo-HL.png',
+    LINK_ICON_BANG: 'https://i.postimg.cc/xjzpvXCm/Logo-HL.png',
+    LINK_ICON_DANG_TAI: 'https://i.postimg.cc/xjzpvXCm/Logo-HL.png',
+    LINK_ICON_TRONG: 'https://i.postimg.cc/xjzpvXCm/Logo-HL.png',
+    LINK_ICON_LAM_MOI: 'https://i.postimg.cc/xjzpvXCm/Logo-HL.png'
+};
+
+// =========================================================================
+// KHỐI 4: CẤU HÌNH XÁC THỰC (GOOGLE IDENTITY)
+// =========================================================================
+const SKT_GOOGLE_CLIENT_ID = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
