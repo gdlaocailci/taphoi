5d5535b951c405b9e122430d74768735433c12fa
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-08-28 09:13:10 +0700

    Add files via upload

---
+++ ketnoi.js
@@ -0,0 +1,27 @@
+/**
+ * TỆP KẾT NỐI HỆ THỐNG (ketnoi.js)
+ * Vị trí lưu trữ: GitHub (Cùng cấp với tệp index.html)
+ * Chức năng: Cấu hình tập trung thông tin định danh và địa chỉ máy chủ (API).
+ */
+
+const KETNOI = {
+    // 1. Thông tin định danh cơ quan/đơn vị
+    TEN_DINH_DANH: "Hợp Thành OS",
+    TIEU_DE_TRANG: "Cổng thông tin Quản trị - Trường TH&THCS Hợp Thành",
+    
+    // 2. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
+    LINK_LOGO: "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png",
+    LINK_HINH_NEN: "https://bigpicturesb.org/wp-content/uploads/2024/10/anh-nen-win-10-51-scaled.jpeg",
+    LINK_AVATAR_NAM: "https://i.postimg.cc/N22XJ7Lz/Nam.png",
+    LINK_AVATAR_NU: "https://i.postimg.cc/9DC4Zjt2/NU.png",
+    LINK_AVATAR_NU_THU_KY: "https://i.postimg.cc/KYCGvkbp/Nu-thu-ky.png",
+    LINK_ICON_TRANG_CHU: "https://i.postimg.cc/xjzpvXCm/Logo-HL.png",
+    // Bổ sung liên kết YouTube Hướng dẫn
+    LINK_YOUTUBE: "https://www.youtube.com/channel/UCdfiTWwv78VITvzlgxnv02Q",
+    
+    // 3. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwatqZkT9zencA2tHQu9xvbjkzduBkZq42jxnqSqFaHEJcBREoWNFVCvUK17BKVe4PHHw/exec",
+
+    // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
+    CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com"
+};
