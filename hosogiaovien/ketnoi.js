b45f5fd8198231b8c4c404ac74233130604dadd1
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-06 09:50:13 +0700

    Add files via upload

---
+++ hosogiaovien/ketnoi.js
@@ -0,0 +1,21 @@
+const KETNOI = {
+    // 1. Thông tin định danh cơ quan/đơn vị
+    TEN_DINH_DANH: "Hợp Thành OS",
+    TIEU_DE_TRANG: "Cổng thông tin Quản trị - Trường TH&THCS Hợp Thành",
+    
+    // 2. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
+    LINK_LOGO: "https://i.postimg.cc/xjzpvXCm/Logo-HL.png",
+    LINK_HINH_NEN: "https://www.wallpaperalchemy.com/storage/wallpapers/2256/windows-10-colorful-3d-logo-wallpaper-card.jpg",
+   LINK_AVATAR_NAM: "https://i.postimg.cc/N22XJ7Lz/Nam.png",
+    LINK_AVATAR_NU: "https://i.postimg.cc/9DC4Zjt2/NU.png",
+    LINK_AVATAR_NU_THU_KY: "https://i.postimg.cc/KYCGvkbp/Nu-thu-ky.png",
+    LINK_ICON_TRANG_CHU: "https://i.postimg.cc/xjzpvXCm/Logo-HL.png",
+    // Bổ sung liên kết YouTube Hướng dẫn
+    LINK_YOUTUBE: "https://www.youtube.com/channel/UCdfiTWwv78VITvzlgxnv02Q",
+    
+    // 3. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxJEriNwHp3twwlVk_yqViktUo96XOj71ciMdNsNY8TSuH_76t7gPt48DGnWU34gWVdeQ/exec",
+
+    // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
+    CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com"
+};
