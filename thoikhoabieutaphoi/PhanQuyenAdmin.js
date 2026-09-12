1253094d661648df9f8798a8407817c5709d6606
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-09-12 17:28:54 +0700

    Update PhanQuyenAdmin.js

--- thoikhoabieutaphoi/PhanQuyenAdmin.js
+++ thoikhoabieutaphoi/PhanQuyenAdmin.js
@@ -18,6 +18,7 @@ const DANH_SACH_NUT_CHUC_NANG = [
     {id: 'btnLuuCoDinh', ten: 'TKB Cố Định'},
     {id: 'btnXepTuDong', ten: 'Xếp Tự Động'},
     {id: 'btnKiemTra', ten: 'Định Mức tiết'}
+    {id: 'btnChuyenTuan', ten: 'Mũi tên Chuyển tuần'}
 ];
 
 // Khởi tạo và Bơm Giao diện vào index.html lúc tải trang
